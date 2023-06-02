e730011980ce6a4d062562fb547693424f9365a8
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2023-06-02 15:51:25 -0500

    feat: create form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import styles from './Home.module.css';
+import Image from "next/image";
+
+const text = "¡Obtén el impulso financiero que necesitas hoy mismo con nuestros préstamos rápidos y confiables!";
+const description = "Tu solución financiera a un solo clic: descubre nuestros préstamos flexibles y asequibles para alcanzar tus metas económicas.";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <>
-      <h4>Home</h4>
-    </>
+    <section className={styles.landing}>
+      <div className={`${styles.item} ${styles.flexColumn}`}>
+        <h2>{text} </h2>
+        <p className={styles.description}>{description}</p>
+        <div className="flex flex-row gap-5 w-100">
+          <input
+            className="w-80 h-12 px-3 placeholder-gray-500 border rounded-lg focus:shadow-outline"
+            type="text"
+            placeholder="Ingresa tu correo electrónico"
+          />
+          <button className="w-30 h-12 px-6 text-white bg-blue-600 rounded-lg hover:bg-blue-700" onClick={() => router.push('/register')}>
+            Solicitar
+          </button>
+        </div>
+      </div>
+      <Image className={styles.item}
+        src="/images/coins.svg"
+        alt="landing"
+        width={300}
+        height={300}
+      />
+    </section>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,379 @@
+import { cities } from '../../utils/cities'
+import { PhotoIcon } from '@heroicons/react/24/solid'
+
+
 export default function RegisterPage() {
-  return <><h1>Register</h1></>
+  return (
+    <form>
+      <div className="border-b border-gray-900/10 pb-12">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">Información Personal</h2>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-rows md:grid-cols-6">
+          <div className="md:col-span-2">
+            <label htmlFor="document-type" className="block text-sm font-medium leading-6 text-gray-900">
+              Tipo de documento
+            </label>
+            <div className="mt-2">
+              <select
+                id="document-type"
+                name="document-type"
+                autoComplete="document-type"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="CC">Cédula de ciudadanía</option>
+                <option value="CE">Cédula de extranjería</option>
+                <option value="PA">Pasaporte</option>
+                <option value="TI">Tarjeta de identidad</option>
+              </select>
+            </div>
+          </div>
+          <div className="md:col-span-2">
+            <label htmlFor="dni" className="block text-sm font-medium leading-6 text-gray-900">
+              Número de documento
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="dni"
+                id="dni"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="md:col-span-2">
+            <label htmlFor="optional-identifier" className="block text-sm font-medium leading-6 text-gray-900">
+              Identificador opcional
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="optional-identifier"
+                id="optional-identifier"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+           <div className="md:col-span-3">
+            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Primer nombre
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="md:col-span-3">
+            <label htmlFor="second-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Segundo nombre
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="second-name"
+                id="second-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="md:col-span-3">
+            <label htmlFor="first-last-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Primer apellido
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-last-name"
+                id="first-last-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="md:col-span-3">
+            <label htmlFor="second-last-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Segundo apellido
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="second-last-name"
+                id="second-last-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="md:col-span-3">
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              Correo electrónico
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className='md:col-span-1'>
+            <label htmlFor="call-sign" className="block text-sm font-medium leading-6 text-gray-900">
+              Indicativo
+            </label>
+            <div className="mt-2">
+              <select
+                name="call-sign"
+                id="call-sign"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="57">+57</option>
+                <option value="58">+58</option>
+                <option value="593">+593</option>
+                <option value="51">+51</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="md:col-span-2">
+            <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
+              Número de teléfono
+            </label>
+            <div className="mt-2">
+              <input
+                type="tel"
+                name="phone-number"
+                id="phone-number"
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+              />
+            </div>
+          </div>
+
+          <div className="md:col-span-3">
+            <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+              Dirección
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="street-address"
+                id="street-address"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="md:col-span-2">
+            <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+              Ciudad
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="md:col-span-1">
+            <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
+              ZIP / Código postal
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="postal-code"
+                id="postal-code"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className='md:col-span-3'>
+            <label htmlFor="job-address" className="block text-sm font-medium leading-6 text-gray-900">
+              Dirección de trabajo
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="job-address"
+                id="job-address"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div> 
+        </div>
+      </div>
+      <div className="border-b border-gray-900/10 pb-12">
+        <h2 className="text-base font-semibold leading-7 text-gray-900 pt-12">Información del Préstamo</h2>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-2">
+            <label htmlFor="currency-type" className="block text-sm font-medium leading-6 text-gray-900">
+              Tipo de moneda
+            </label>
+            <div className="mt-2">
+              <select
+                id="currency-type"
+                name="currency-type"
+                autoComplete="currency-type"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="COP">COP</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+              </select>
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="amount" className="block text-sm font-medium leading-6 text-gray-900">
+              Monto
+            </label>
+            <div className="mt-2">
+              <input
+                type="number"
+                name="amount"
+                id="amount"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Ingresa el monto"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="deadline" className="block text-sm font-medium leading-6 text-gray-900">
+              Plazos de pago
+            </label>
+            <div className="mt-2">
+              <select
+                id="deadline"
+                name="deadline"
+                autoComplete="deadline"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="1">1 mes</option>
+                <option value="2">2 meses</option>
+                <option value="3">3 meses</option>
+                <option value="4">4 meses</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b border-gray-900/10 pb-12">
+        <h2 className="text-base font-semibold leading-7 text-gray-900 pt-12">Soportes documentales</h2>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="col-span-3">
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Documento de identificación - Anverso
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Subir archivo</span>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  </label>
+                  <p className="pl-1">o arrastra y suelta</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">PNG, JPG hasta 10MB</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3">
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Documento de identificación - Reverso
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Subir archivo</span>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  </label>
+                  <p className="pl-1">o arrastra y suelta</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">PNG, JPG hasta 10MB</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2 md:col-span-3">
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Salario - Soporte
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Subir archivo</span>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  </label>
+                  <p className="pl-1">o arrastra y suelta</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">PNG, JPG hasta 10MB</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2 md:col-span-3">
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Salario - Soporte último mes
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Subir archivo</span>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  </label>
+                  <p className="pl-1">o arrastra y suelta</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">PNG, JPG hasta 10MB</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2 sm:col-span-full">
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Servicios públicos - Soporte
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Subir archivo</span>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  </label>
+                  <p className="pl-1">o arrastra y suelta</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">PNG, JPG hasta 10MB</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    </form>
+  );
 }

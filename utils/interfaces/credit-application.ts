aa1adac95fd81770d49d98d08c91c473b08770c8
Miguel Henao <henaoariasjm@gmail.com>
2023-07-07 11:48:23 -0500

    fix: update interface

--- utils/interfaces/credit-application.ts
+++ utils/interfaces/credit-application.ts
@@ -11,21 +11,24 @@ export interface CreditApplicationResponsePayload {
   createdAt: Date;
   updatedAt: Date;
   clientDNI: string;
-  tasaInteres: 0,
+  tasaInteres: 0;
+  plazoMaximo: 1;
 }
 
 export interface CreditApplicationRequestPayload {
   id?: number;
   tipo_moneda: string;
   valor_credito_total: number;
+  tasa_interes: 0;
   plazos_cuotas: number;
-  estado_aprobado_interno: number;
+  plazo_maximo: number;
+  preapobado_form_tiempos: number;
   estado_externo: number;
   fecha_aprobacion: string | null;
   created_at: string;
   updated_at: string;
   cedula_cliente: string;
-  tasa_interes: 0,
+  approved_by: string;
 }
 
 export class CreditApplicationRequestMapper {
@@ -37,13 +40,14 @@ export class CreditApplicationRequestMapper {
       currencyType: payload.tipo_moneda,
       amount: payload.valor_credito_total,
       deadline: payload.plazos_cuotas,
-      internalApproved: payload.estado_aprobado_interno,
+      internalApproved: payload.preapobado_form_tiempos,
       externalApproved: payload.estado_externo,
       approvalDate: new Date(payload.fecha_aprobacion || ''),
       createdAt: new Date(payload.created_at),
       updatedAt: new Date(payload.updated_at),
       clientDNI: payload.cedula_cliente,
-      tasaInteres: 0
+      tasaInteres: 0,
+      plazoMaximo: 1
     };
   }
 
@@ -55,13 +59,15 @@ export class CreditApplicationRequestMapper {
       tipo_moneda: payload.currencyType,
       valor_credito_total: payload.amount,
       plazos_cuotas: payload.deadline,
-      estado_aprobado_interno: payload.internalApproved,
+      preapobado_form_tiempos: payload.internalApproved,
       estado_externo: payload.externalApproved,
       fecha_aprobacion: payload.approvalDate?.toISOString() || null,
       created_at: payload.createdAt.toISOString(),
       updated_at: payload.updatedAt.toISOString(),
       cedula_cliente: payload.clientDNI,
-      tasa_interes: 0
+      tasa_interes: 0,
+      plazo_maximo: 1,
+      approved_by: ''
     };
   }
 }

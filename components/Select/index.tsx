9473593f048e77c87df4f4032e061d97b8eddf3e
Miguel Henao <henaoariasjm@gmail.com>
2023-07-18 16:22:00 -0500

    feat: add Input and Select components

---
+++ components/Select/index.tsx
@@ -0,0 +1,43 @@
+import { LegacyRef, forwardRef } from 'react';
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
+import styles from '../Input/Input.module.css';
+
+interface Props {
+  label: string;
+  hasError: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined;
+  id?: string;
+  colSpan: number;
+  selectLabel?: string;
+  selectValue?: string;
+  options: any[];
+}
+
+function Select({ label, id, hasError, colSpan, selectLabel, selectValue, options, ...props }: Props, ref: LegacyRef<HTMLSelectElement>) {
+  return (
+    <div className={`md:col-span-${colSpan}`}>
+      <label htmlFor={id} className={`${styles.label} block text-sm font-medium leading-6 text-gray-900`}>
+        {label}
+      </label>
+      <div className="mt-2">
+        <select
+          {...props}
+          ref={ref}
+          id="document-type"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        >
+          {options.map((option) => (
+            <option
+              key={selectValue ? option[selectValue] : option}
+              value={selectValue ? option[selectValue] : option}
+            >
+              {selectLabel ? option[selectLabel] : option}
+            </option>
+          ))}
+        </select>
+        {hasError && <span className='text-red-500 text-xs'>Este campo es requerido</span>}
+      </div>
+    </div>
+  );
+}
+
+export default forwardRef(Select);

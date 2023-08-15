7a0ce1da729f3ac427dbf8eac657d6aad3f571bb
Miguel Henao <henaoariasjm@gmail.com>
2023-08-14 23:51:52 -0500

    fix: update tables to cards

--- components/Table/ApplicationsTable/index.tsx
+++ components/Table/ApplicationsTable/index.tsx
@@ -1,45 +1,48 @@
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react';
 import { durationText } from '../../../utils/constants/credit-duration';
 import { deadlinesText } from '../../../utils/constants/deadlines';
-import { status, statusBg } from '../../../utils/constants/status';
+import { status } from '../../../utils/constants/status';
 
 interface Props {
   data: any[];
 }
 
 function ApplicationsTable({ data }: Props) {
-  const columns = [
-    { key: 'created_at', label: 'Fecha de solicitud' },
-    { key: 'cedula_cliente', label: 'Número de documento' },
-    { key: 'duracion_credito', label: 'Duración del crédito' },
-    { key: 'cuotas_credito', label: '¿Cada cuánto se pagará el crédito?' },
-    { key: 'valor_credito_total', label: 'Monto solicitado' },
-    { key: 'preapobado_form_tiempos', label: 'Estado de la solicitud' },
-  ];
+  const application = data[0];
 
   return (
-    <>
-      <h4 className='text-xl'>Solicitud</h4>
-      <Table aria-label="Example static collection table">
-        <TableHeader>
-          {columns.map(({ label }, index) => (
-            <TableColumn key={index} className='text-center'>{label}</TableColumn>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index} className={statusBg[row['preapobado_form_tiempos']]}>
-              <TableCell key='created_at' className='text-center'>{formatDate(row['created_at'])}</TableCell>
-              <TableCell key='cedula_cliente' className='text-center'>{row['cedula_cliente']}</TableCell>
-              <TableCell key='duracion_credito' className='text-center'>{durationText[row['duracion_credito']]}</TableCell>
-              <TableCell key='cuotas_credito' className='text-center'>{deadlinesText[row['cuotas_credito']]}</TableCell>
-              <TableCell key='valor_credito_total' className='text-center'>{row['valor_credito_total']} UYU</TableCell>
-              <TableCell key='preapobado_form_tiempos' className='text-center'>{status[row['preapobado_form_tiempos']]}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
+    <div className="bg-white shadow-md">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-2 py-12 sm:px-3 sm:py-16 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Solicitud</h2>
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-2 sm:gap-y-8 lg:gap-x-8">
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Fecha de solicitud</dt>
+              <dd className="mt-2 text-sm text-gray-500">{formatDate(application['created_at'])}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Número de documento</dt>
+              <dd className="mt-2 text-sm text-gray-500">{application['cedula_cliente']}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Duración del crédito</dt>
+              <dd className="mt-2 text-sm text-gray-500">{durationText[application['duracion_credito']]}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">¿Cada cuánto se pagará el crédito?</dt>
+              <dd className="mt-2 text-sm text-gray-500">{deadlinesText[application['cuotas_credito']]}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Estado de la solicitud</dt>
+              <dd className="mt-2 text-sm text-gray-500">{status[application['preapobado_form_tiempos']]}</dd>
+            </div>
+          </dl>
+        </div>
+        <div className="bg-yellow-100 shadow-md py-28 grid grid-cols-1 gap-4 sm:gap-6 lg:gap-8">
+          <h3 className="text-xl tracking-tight text-gray-500 sm:text-xl">Monto solicitado</h3>
+          <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">{application['valor_credito_total']} UYU</h3>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- components/Table/CreditsTable/index.tsx
+++ components/Table/CreditsTable/index.tsx
@@ -1,4 +1,3 @@
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react';
 import { status } from '../../../utils/constants/status';
 
 interface Props {
@@ -6,44 +5,47 @@ interface Props {
 }
 
 function CreditsTable({ data }: Props) {
-  const columns = [
-    { key: 'id', label: '#' },
-    { key: 'balance', label: 'Balance' },
-    { key: 'fecha_inicio', label: 'Fecha de inicio' },
-    { key: 'fecha_vencimiento_general', label: 'Fecha de vencimiento' },
-    { key: 'descripcion', label: 'Descripción' },
-    { key: 'estado', label: 'Estado' },
-  ];
+  const credit = data[0];
 
   return (
-    <>
-      <h4 className='text-xl'>Crédito</h4>
-      <Table>
-        <TableHeader>
-          {columns.map(({ label }, index) => (
-            <TableColumn key={index} className='text-center'>{label}</TableColumn>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell key='id' className='text-center'>{row['id']}</TableCell>
-              <TableCell key='balance' className='text-center'>{row['pagos_total']} UYU / {row['valor_credito_total']} UYU</TableCell>
-              <TableCell key='fecha_inicio' className='text-center'>{formatDate(row['fecha_inicio'])}</TableCell>
-              <TableCell key='fecha_vencimiento_general' className='text-center'>{formatDate(row['fecha_vencimiento_general'])}</TableCell>
-              <TableCell key='descripcion' className='text-center'>{row['descripcion_adicional']}</TableCell>
-              <TableCell key='estado' className='text-center'>{status[row['preapobado_form_tiempos']]}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <h4 className='text-xl'>Periodos de pago</h4>
-    </>
+    <div className="bg-white shadow-md">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-2 py-12 sm:px-3 sm:py-16 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div className="bg-blue-100 shadow-md py-28 grid grid-cols-1 gap-4 sm:gap-6 lg:gap-8">
+          <h3 className="text-xl tracking-tight text-gray-500 sm:text-xl">Balance</h3>
+          <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">{credit['pagos_total'] ?? 0} UYU / {credit['valor_credito_total']} UYU</h3>
+        </div>
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Crédito</h2>
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-2 sm:gap-y-8 lg:gap-x-8">
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Número de crédito</dt>
+              <dd className="mt-2 text-sm text-gray-500">{credit['id']}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Fecha de inicio</dt>
+              <dd className="mt-2 text-sm text-gray-500">{formatDate(credit['fecha_inicio'])}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Fecha de vencimiento</dt>
+              <dd className="mt-2 text-sm text-gray-500">{formatDate(credit['fecha_vencimiento_general'])}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Descripción</dt>
+              <dd className="mt-2 text-sm text-gray-500">{credit['descripcion_adicional']}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-2">
+              <dt className="font-medium text-gray-900">Estado del crédito</dt>
+              <dd className="mt-2 text-sm text-gray-500">{status[credit['preapobado_form_tiempos']]}</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
   );
 }
 
 
-function formatDate(date: string) {  
+function formatDate(date: string) {
   return new Date(date).toLocaleDateString();
 }
 
